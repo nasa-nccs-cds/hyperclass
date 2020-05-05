ff26ff9ca403e6484ef14704af29c24691970eed
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-05-05 10:33:49 -0400

    Update test

--- hyperclass/umap/exe/dev/fit_stability.py
+++ hyperclass/umap/exe/dev/fit_stability.py
@@ -14,23 +14,20 @@ if __name__ == '__main__':
 
     image_name = "ang20170720t004130_corr_v2p9"
     refresh = True
-    plot_data = [ ]
     subsample = 5
-    colors = ['#0000ff', '#00ff00', '#ff0000', '#000000']
+    colors =[ '#0000ff', '#00ff00', '#ff0000', '#000000' ]
+    block_indices = [ [0,0], [0,1], [1,0], [1,1] ]
 
     dm = DataManager( image_name )
     tile: Tile = dm.getTile()
+    umgr = UMAPManager(tile, refresh=refresh)
 
-    block1: Block = tile.getBlock( 0,0 )
-    umgr = UMAPManager( tile, refresh = refresh )
-    umgr.fit( block = block1, subsample = subsample )
-    points1 = umgr.mapper.embedding_
-    plot_data.append( go.Scatter3d( x=points1[:,0], y=points1[:,1], z=points1[:,2], mode='markers',  marker= dict( color = colors[0], size=1 ) ) )
-
-    block2: Block = tile.getBlock( 1,1 )
-    umgr.fit( block = block2, subsample = subsample )
-    points2 = umgr.mapper.embedding_
-    plot_data.append( go.Scatter3d(x=points2[:,0], y=points2[:,1], z=points2[:,2], mode='markers',  marker= dict( color = colors[2], size=1 ) ) )
+    plot_data = [ ]
+    for color, block_index in zip(colors,block_indices):
+        block: Block = tile.getBlock( *block_index )
+        umgr.fit( block = block, subsample = subsample )
+        points1 = umgr.mapper.embedding_
+        plot_data.append( go.Scatter3d( x=points1[:,0], y=points1[:,1], z=points1[:,2], mode='markers',  marker= dict( color = color, size=1 ) ) )
 
     fig = go.Figure( data=plot_data )
     fig.show()
