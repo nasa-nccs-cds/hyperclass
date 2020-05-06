6ee7316b6bfc2b4f54a17ca281de9e86b097510e
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-05-06 18:00:28 -0400

    Add graph development

--- hyperclass/umap/exe/dev/nnd_test.py
+++ hyperclass/umap/exe/dev/nnd_test.py
@@ -1,6 +1,7 @@
 from pynndescent import NNDescent
 import xarray as xa
 import numpy as np
+import numpy.ma as ma
 from typing import List, Union, Tuple, Optional
 from hyperclass.data.aviris.manager import DataManager, Tile, Block
 from hyperclass.umap.manager import UMAPManager
@@ -27,22 +28,32 @@ if __name__ == '__main__':
     n_trees = 5 + int(round((graph_nodes.shape[0]) ** 0.5 / 20.0))
     n_iters = max(5, int(round(np.log2(graph_nodes.shape[0]))))
     nnd = NNDescent( graph_nodes, n_trees=n_trees, n_iters=n_iters, n_jobs = -1, n_neighbors=n_neighbors, max_candidates=60, verbose=True )
-    knn_indices, knn_dists = nnd.neighbor_graph
+    I: np.ndarray = nnd.neighbor_graph[0]
+    D: np.ndarray = nnd.neighbor_graph[1]
     t2 = time.time()
-    print(f"Completed computing graph in {(t2 - t1)} sec, index shape = {knn_indices.shape}, dist shape = {knn_dists.shape}")
+    print(f"Completed computing graph in {(t2 - t1)} sec, index shape = {I.shape}, dist shape = {D.shape}")
 
-    I = xa.DataArray( knn_indices, dims=['samples', 'neighbors'], coords = dict( samples = graph_nodes.coords['samples'], neighbors=np.arange(knn_indices.shape[1]) ) )
-    D = xa.DataArray( knn_dists,   dims=['samples', 'neighbors'], coords = dict( samples = graph_nodes.coords['samples'], neighbors=np.arange(knn_indices.shape[1])))
-    test_labels =  xa.full_like( graph_nodes[:,0], float("nan") )
-    P = xa.full_like( test_labels, float("nan") )
+    test_labels = ma.masked_equal( np.full( graph_nodes.shape[:1], -1 ), -1 )
+    P: ma.MaskedArray  = ma.masked_invalid( np.full( graph_nodes.shape[:1], float("nan") ) )
+
     for iL in range(3):
         index = iL*100
         test_labels[index] = iL
         P[index] = 0.0
 
-    I0 = I.unstack().astype( np.int64 )
-    index = I0.stack( neighbor_indices=[...] )
-    PN = P[ index ].unstack()
+    PN0: np.ndarray  = P[ I.flatten() ]
+    PN: np.ndarray = PN0.reshape( I.shape ) + D
+    best_neighbors: ma.MaskedArray = ma.argmin( PN, axis=1 )
+# #    I = xa.DataArray( knn_indices, dims=['samples', 'neighbors'], coords = dict( samples = graph_nodes.coords['samples'], neighbors=np.arange(knn_indices.shape[1]) ) )
+# #    D = xa.DataArray( knn_dists,   dims=['samples', 'neighbors'], coords = dict( samples = graph_nodes.coords['samples'], neighbors=np.arange(knn_indices.shape[1])))
+#     test_labels =  xa.full_like( graph_nodes[:,0], float("nan") )
+#     P = xa.full_like( test_labels, float("nan") )
+#     for iL in range(3):
+#         index = iL*100
+#         test_labels[index] = iL
+#         P[index] = 0.0
+# 
+#     PN = P[ knn_indices.flatten() ].res
 
     print( ". " )
 
