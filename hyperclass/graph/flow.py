18a04d9a482915c86a11e6e7a25b6c2b7f3ca685
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-05-09 13:47:09 -0400

    Integrate flow map viz

--- hyperclass/graph/flow.py
+++ hyperclass/graph/flow.py
@@ -13,6 +13,7 @@ class ActivationFlow:
         self.I: np.ndarray = None
         self.D: ma.MaskedArray = None
         self.P: ma.MaskedArray = None
+        self.C: ma.MaskedArray = None
         self.n_neighbors: int = kwargs.get( 'n_neighbors', 10 )
 
     def setGraph( self, I: np.ndarray, D: np.ndarray ):
@@ -30,30 +31,33 @@ class ActivationFlow:
     def spread( self, sample_labels: xa.DataArray, nIter: int, **kwargs ) -> xa.DataArray:
         debug = kwargs.get( 'debug', False )
         reset = kwargs.get( "reset", False)
-        C = sample_labels.to_masked_array()
-        if (self.P is None) or reset:  self.P = ma.masked_array(  np.full( C.shape, 0.0 ), mask = C.mask )
+        label_mask = sample_labels.isnull().values
+        if self.C is None or reset:  self.C = sample_labels.to_masked_array()
+        else:                        self.C = np.ma.where( label_mask, self.C, sample_labels )
+        if (self.P is None) or reset:   self.P = ma.masked_array(  np.full( self.C.shape, 0.0 ), mask = self.C.mask )
+        else:                           self.P = np.ma.where( label_mask, self.P, 0.0 )
         index0 = np.arange( self.I.shape[0] )
         max_flt = np.finfo( self.P.dtype ).max
-        label_count = C.count()
+        label_count = self.C.count()
         print(f"Beginning graph flow iterations, #C = {label_count}")
-        if debug: print(f"I = {self.I}" ); print(f"D = {self.D}" ); print(f"P = {self.P}" ); print(f"C = {C}" )
+        if debug: print(f"I = {self.I}" ); print(f"D = {self.D}" ); print(f"P = {self.P}" ); print(f"C = {self.C}" )
         t0 = time.time()
         for iter in range(nIter):
             PN: ma.MaskedArray = self.P[ self.I.flatten() ].reshape(self.I.shape) + self.D
-            CN: ma.MaskedArray = C[ self.I.flatten() ].reshape( self.I.shape )
+            CN: ma.MaskedArray = self.C[ self.I.flatten() ].reshape( self.I.shape )
             best_neighbors: ma.MaskedArray = PN.argmin(axis=1, fill_value=max_flt)
             self.P = PN[index0, best_neighbors]
-            C = CN[index0, best_neighbors]
-            new_label_count = C.count()
+            self.C = CN[index0, best_neighbors]
+            new_label_count = self.C.count()
             if new_label_count == label_count:
                 print( "Converged!" )
             else:
                 label_count = new_label_count
                 print(f"\n -->> Iter{iter + 1}: #C = {label_count}\n")
-                if debug: print(f"PN = {PN}"); print(f"CN = {CN}"); print(f"best_neighbors = {best_neighbors}"); print( f"P = {self.P}" ); print( f"C = {C}" )
+                if debug: print(f"PN = {PN}"); print(f"CN = {CN}"); print(f"best_neighbors = {best_neighbors}"); print( f"P = {self.P}" ); print( f"C = {self.C}" )
 
         t1 = time.time()
         print(f"Completed graph flow {nIter} iterations in {(t1 - t0)} sec")
-        return xa.DataArray( C, dims=sample_labels.dims, coords=sample_labels.coords, attrs=sample_labels.attrs )
+        return xa.DataArray( self.C, dims=sample_labels.dims, coords=sample_labels.coords, attrs=sample_labels.attrs )
 
 
