89b8e576293e327bc6ee2723634161d97898f438
Thomas Maxwell <thomas.maxwell@nasa.gov>
2020-06-16 09:53:10 -0400

    Update Indiana Pines

--- hyperclass/exe/console.py
+++ hyperclass/exe/console.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets
 from hyperclass.gui.application import HyperclassConsole
+from hyperclass.data.aviris.manager import dataManager
 import sys
 
 classes = [ ('BareEarth', [1.0, 1.0, 0.0, 1.0]),
@@ -11,5 +12,6 @@ valid_bands = [[3, 193], [210, 287], [313, 421]]
 
 app = QtWidgets.QApplication(sys.argv)
 hyperclass = HyperclassConsole( classes, valid_bands=valid_bands )
+dataManager.config.setValue( 'tile/indices', [1,1] )
 hyperclass.show()
 sys.exit(app.exec_())
